8f29afdaad0e195c92fe89001eb36a248a5b47d1
Gwanghyeon Gim <harryghgim@gmail.com>
2020-08-08 15:35:08 +0900

    Routes and controllers DONE

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+// Import routes for "catalog" area of site
+var catalogRouter = require('./routes/catalog');
 
 var app = express();
 
@@ -28,6 +30,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+// Add catalog routes to middleware chain.
+app.use('/catalog', catalogRouter); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
